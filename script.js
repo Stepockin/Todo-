8221d5af1512df51d8c4733401965b5f44c2c82d
Daniil Alenichev <proga2024@mail.ru>
2024-09-24 21:01:05 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const list = document.getElementsByClassName("list")[0]
+addDeleteHandlers()
+function addItem(){
+    const itemText = prompt("Что добавить?") || '<div></div>'
+    const item = document.createElement("div")
+    item.innerHTML = `${itemText}<button class = "btn delete-btn">X</button>`
+    list.appendChild(item)
+    addDeleteHandlers()
+}
+function clearList(){
+    list.innerHTML = "";
+}
+function deleteItem(event) {  
+    event.target.parentElement.remove()
+}
+function addDeleteHandlers(){
+    const deleteButtons = document.getElementsByClassName("delete-btn")
+    for(let i = 0; i < deleteButtons.length;i++){
+        const deleteButton = deleteButtons[i];
+        deleteButton.addEventListener("click",deleteItem)
+    }
+}
